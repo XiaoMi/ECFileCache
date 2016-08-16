73c7a22e35cf3a14eb5154abb9ba042902cc1259
guoxuedong <guoxuedong@xiaomi.com>
2016-08-16 14:44:55 +0800

    fix appconfig

--- ECFileCache-test/src/main/java/com/xiaomi/filecache/ec/AppConfig.java
+++ ECFileCache-test/src/main/java/com/xiaomi/filecache/ec/AppConfig.java
@@ -1,14 +1,17 @@
 package com.xiaomi.filecache.ec;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class AppConfig {
@@ -36,24 +39,28 @@ public class AppConfig {
         }
 
         Properties prop = new Properties();
+        InputStream inputStream = null;
         try {
             if (readFile) {
                 if (!new File(profile).exists()) {
                     throw new IllegalArgumentException("the config property file does not exist: " + profile);
                 }
-                prop.load(new FileReader(profile));
+                inputStream = new FileInputStream(profile);
             } else {
-                prop.load(AppConfig.class.getResourceAsStream(profile));
+                inputStream = AppConfig.class.getResourceAsStream(profile);
             }
+            prop.load(inputStream);
         } catch (FileNotFoundException e) {
             LOGGER.error("config file path is unavailable. profile: {}", profile);
-            throw new IllegalArgumentException("config file path is unavailable.");
+            throw new IllegalArgumentException("config file path is unavailable.", e);
         } catch (IOException e) {
             LOGGER.error("found IOException while loading file. profile: {}", profile);
-            throw new IllegalArgumentException("found IOException while loading file.");
+            throw new IllegalArgumentException("found IOException while loading file.", e);
+        } finally {
+            IOUtils.closeQuietly(inputStream);
         }
 
-        threadNum= Integer.parseInt(prop.getProperty("thread.num", "1"));
+        threadNum = Integer.parseInt(prop.getProperty("thread.num", "1"));
         Validate.isTrue(threadNum > 0);
 
         fileSize= Integer.parseInt(prop.getProperty("file.size", "1024"));
